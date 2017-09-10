61468cc780019c49078ddcaa108e092f706ec655
flo <flo@cis.lmu.de>
2017-09-10 17:16:30 +0200

    implement fuzzy test case

--- sparsetabledfa_test.go
+++ sparsetabledfa_test.go
@@ -2,6 +2,8 @@ package fsa
 
 import (
 	"bytes"
+	"fmt"
+	"math/rand"
 	"sort"
 	"testing"
 )
@@ -99,6 +101,63 @@ func TestEachTransition(t *testing.T) {
 	for _, c := range []byte{'a', 'v', 'd', 'f', 's'} {
 		if !chars[c] {
 			t.Errorf("expected chars to contain %c", c)
+		}
+	}
+}
+
+var chars = []rune{
+	'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+	'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+	'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+	'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+	'ä', 'ö', 'ü', 'ß', 'Ä', 'Ö', 'Ü',
+	',', '.', '~', '[', ']', '{', '}', '(', ')', ':', '!', '?', ' ',
+}
+
+func makeRandomString(r *rand.Rand) string {
+	n := r.Intn(100)
+	var str string
+	for i := 0; i < n; i++ {
+		x := r.Intn(len(chars))
+		str = fmt.Sprintf("%s%c", str, chars[x])
+	}
+	return str
+}
+
+func makeRandomStrings(n int, r *rand.Rand) (map[string]bool, []string) {
+	m := make(map[string]bool)
+	var s []string
+	for i := 0; i < n; i++ {
+		str := makeRandomString(r)
+		if !m[str] {
+			m[str] = true
+			s = append(s, str)
+		}
+	}
+	sort.Slice(s, func(i, j int) bool {
+		return bytes.Compare([]byte(s[i]), []byte(s[j])) < 0
+	})
+	return m, s
+}
+
+func TestFuzzy(t *testing.T) {
+	seed := rand.Int63()
+	r := rand.New(rand.NewSource(seed))
+	m, s := makeRandomStrings(100, r)
+	b := NewSparseTableDFABuilder()
+	for _, str := range s {
+		if err := b.Add(str, 1); err != nil {
+			t.Fatalf("could not add string: %v (%d)", err, seed)
+		}
+	}
+	dfa := b.Build()
+	for i := 0; i < 10000; i++ {
+		str := makeRandomString(r)
+		if accepts(dfa, str) && !m[str] {
+			t.Errorf("dfa accepts %q; but it shouldn't (%d)", str, seed)
+		}
+		if !accepts(dfa, str) && m[str] {
+			t.Errorf("dfa does not accept %q; but it should (%d)", str, seed)
 		}
 	}
 }
