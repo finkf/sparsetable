5692126c7b573b1de53a9948c1a0664feb924af7
flo <flo@cis.lmu.de>
2017-09-11 11:24:50 +0200

    make go lint happy

--- sptdfa.go
+++ sptdfa.go
@@ -6,22 +6,25 @@ import (
 	"unicode"
 )
 
+// SparseTableDFA is a DFA implementation using a sparse table.
 type SparseTableDFA struct {
 	table   []Cell
 	initial uint32
 }
 
+// Initial returns the initial state of the DFA.
 func (d *SparseTableDFA) Initial() uint32 {
 	return d.initial
 }
 
+// Delta makes on transition from the given state s with the given byte c.
 func (d *SparseTableDFA) Delta(s uint32, c byte) uint32 {
 	n := uint32(len(d.table))
 	o := uint32(c)
 	if s <= 0 || s > n || s+o > n {
 		return 0
 	}
-	s -= 1
+	s--
 	if !d.table[s].State() ||
 		d.table[s+o].typ != transitionCellType ||
 		d.table[s+o].char != c {
@@ -30,6 +33,8 @@ func (d *SparseTableDFA) Delta(s uint32, c byte) uint32 {
 	return d.table[s+o].data + 1
 }
 
+// Final returns the (data, true) if the given state is final.
+// If the given state is not final, (0, false) is returned.
 func (d *SparseTableDFA) Final(s uint32) (uint32, bool) {
 	n := uint32(len(d.table))
 	if s <= 0 || n <= s || d.table[s-1].typ != finalCellType {
@@ -38,6 +43,7 @@ func (d *SparseTableDFA) Final(s uint32) (uint32, bool) {
 	return d.table[s-1].data, true
 }
 
+// EachTransition iterates over all transitions of the given state.
 func (d *SparseTableDFA) EachTransition(s uint32, f func(Cell)) {
 	n := uint32(len(d.table))
 	if s <= 0 || s > n || !d.table[s-1].State() {
@@ -52,6 +58,7 @@ func (d *SparseTableDFA) EachTransition(s uint32, f func(Cell)) {
 	}
 }
 
+// Dot prints out the dotcode for the DFA.
 func (d *SparseTableDFA) Dot(out io.Writer) {
 	dot := "// dotcode\n"
 	fmt.Fprintf(out, "digraph dfa { %s", dot)
@@ -67,17 +74,16 @@ func (d *SparseTableDFA) Dot(out io.Writer) {
 			fmt.Fprintf(out, " %d[peripheries=1] %s", i, dot)
 		case transitionCellType:
 			fmt.Fprintf(out, " %d -> %d [label=%q] %s",
-				i-int(cell.char), cell.data, str(cell.char), dot)
+				i-int(cell.char), cell.data, byte2string(cell.char), dot)
 		case emptyCellType:
 		}
 	}
 	fmt.Fprintf(out, "} %s", dot)
 }
 
-func str(c byte) string {
+func byte2string(c byte) string {
 	if c < 0x80 && unicode.IsPrint(rune(c)) {
 		return fmt.Sprintf("%c", c)
-	} else {
-		return fmt.Sprintf("0x%x", c)
 	}
+	return fmt.Sprintf("0x%x", c)
 }
